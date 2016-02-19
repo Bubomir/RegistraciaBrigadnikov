5b3f5e27a7adf30ac814e609063cbc3cf71d344e
Jozef Bíroš <jozef.biros91@gmail.com>
2016-02-19 22:40:13 +0100

    Cleaning a code

--- registration.php
+++ registration.php
@@ -1,35 +1,28 @@
 <?php
     include_once 'dbconnect.php';
 
-        $first_name = $_POST['first_name'];
-        $surname = $_POST['surname'];
-        $email =  $email = $_POST['email'];
-        $permission = $_POST['permissions'];
-        $tempPass = md5(rand(1000, 1000000));
+    //Variabiles acquired from ajax throught registration form
+    $first_name = $_POST['first_name'];
+    $surname = $_POST['surname'];
+    $email =  $email = $_POST['email'];
+    $permission = $_POST['permissions'];
+    $tempPass = md5(rand(1000, 100000));
 
-        if(mysqli_query($db,"INSERT INTO $table_employees(First_Name,Surname,Password,Email,Permissions) VALUES('$first_name','$surname','$tempPass','$email','$permission')")){
-            echo true;
+    if(mysqli_query($db,"INSERT INTO $table_employees(First_Name,Surname,Password,Email,Permissions) VALUES('$first_name','$surname','$tempPass','$email','$permission')")){
+        echo true;
+
+        //Email content
         $to = $email;
-        $subject = 'Registracia brigadnika';
-        $message = "Prihlasovacie udaje:
-                        Prihlasovaci email: $email
+        $subject = 'Vytvoření hesla pro váš účet';
+        $message = "Přihlašovací e-mail: $email <br><br>
+        Zde klikněte pro vytvoření vašeho hesla a dokončení vaší registrace: <br>
+        http://vtstudentplanner.cz/createPass.php?tempPass=$tempPass&email=$email";
+        $headers = 'From: noreply@vtstudentplanner.cz'."\r\n" . 'Content-type:text/html;charset=UTF-8' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
 
-                    Kliknite tu pre vytvorenia vasho hesla a dokoncenie registrácie
-                    http://vtstudentplanner.cz/createPass.php?tempPass=$tempPass&email=$email";
+        mail($to, $subject, $message, $headers);
+    } else{
+      echo false;
+    }
 
-
-        $headers = 'From: vtstudentplanner.cz'."\r\n" .
-            'Reply-To: ' . $to . "\r\n" .
-            'X-Mailer: PHP/' . phpversion();
-
-            if(!mail($to, $subject, $message, $headers)){
-           //echo 'uspesne odoslany email';
-        }
-        }
-        else{
-          echo false;
-        }
-
-         mysqli_close($db);
-
+    mysqli_close($db);
 ?>
