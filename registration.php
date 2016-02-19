74b2105a7d6fa9efd2ec62b7b015899189ce69a2
Bubo <bubomirxxx@gmail.com>
2016-02-19 19:36:23 +0100

    test commit

---
+++ registration.php
@@ -0,0 +1,35 @@
+<?php
+    include_once 'dbconnect.php';
+
+        $first_name = $_POST['first_name'];
+        $surname = $_POST['surname'];
+        $email =  $email = $_POST['email'];
+        $permission = $_POST['permissions'];
+        $tempPass = md5(rand(1000, 1000000));
+
+        if(mysqli_query($db,"INSERT INTO $table_employees(First_Name,Surname,Password,Email,Permissions) VALUES('$first_name','$surname','$tempPass','$email','$permission')")){
+            echo true;
+        $to = $email;
+        $subject = 'Registracia brigadnika';
+        $message = "Prihlasovacie udaje:
+                        Prihlasovaci email: $email
+
+                    Kliknite tu pre vytvorenia vasho hesla a dokoncenie registrácie
+                    http://localhost/createPass.php?tempPass=$tempPass&email=$email";
+
+
+        $headers = 'From: vtstudentplanner.cz'."\r\n" .
+            'Reply-To: ' . $to . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
+            if(!mail($to, $subject, $message, $headers)){
+           //echo 'uspesne odoslany email';
+        }
+        }
+        else{
+          echo false;
+        }
+
+         mysqli_close($db);
+
+?>
