1d58ce9bf12f6a6eb4b20b2e0d8983e2704d051f
Jozef Bíroš <jozef.biros91@gmail.com>
2016-02-25 21:49:54 +0100

    title fixed

--- home.php
+++ home.php
@@ -18,7 +18,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Foundation</title>
+        <title>VT Student Planner</title>
         <link rel="stylesheet" href="css/app.css" />
         <link rel="stylesheet" href="css/motion-ui.css" />
         <link href="css/fullcalendar.css" rel='stylesheet' />
--- template/head.php
+++ template/head.php
@@ -4,6 +4,6 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>ViskoTeepakLoginForm</title>
+    <title>VT Student Planner</title>
     <link rel="stylesheet" href="css/login.css" />
 </head>
