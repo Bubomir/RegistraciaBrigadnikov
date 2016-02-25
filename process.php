bb004bd0eb66668516004a5ddd12667647f0cb67
Bubo <bubomirxxx@gmail.com>
2016-02-25 21:46:25 +0100

    add sending permissions

--- process.php
+++ process.php
@@ -314,11 +314,12 @@ if($type == 'get_loggedPermissions'){
 }
 if($type == 'mouseOver'){
     $mouse_over_id = $_POST['eventID'];
-    $query = mysqli_query($db, "SELECT Email, Mobile_Number FROM $table_employees INNER JOIN $table_calendar ON $table_calendar.p_Email = $table_employees.Email  WHERE ID = '$mouse_over_id'");
+    $query = mysqli_query($db, "SELECT Permissions, Email, Mobile_Number FROM $table_employees INNER JOIN $table_calendar ON $table_calendar.p_Email = $table_employees.Email  WHERE ID = '$mouse_over_id'");
     $fetch = mysqli_fetch_array($query);
 
     $e = array();
     $event_array = array();
+    $e['Permissions'] = $fetch['Permissions'];
     $e['Email'] = $fetch['Email'];
     $e['Phone_num'] = $fetch['Mobile_Number'];
 
