9a5e46322bdfca088b51982d63fc406537610fa0
Jozef Bíroš <jozef.biros91@gmail.com>
2016-02-25 09:46:10 +0100

    Small fix

--- js/attendanceCalendar.js
+++ js/attendanceCalendar.js
@@ -668,9 +668,9 @@ $(document).ready(function () {
                             refreshEvents();
                         }*/
                     } else {
-                        if (permissions === 'admin') {
+
                             deleteEvent(event);
-                        }
+
                     }
                 }
             }
