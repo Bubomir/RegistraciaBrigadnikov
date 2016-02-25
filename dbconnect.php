3166980f64d0208d0058bf85804d5d664f284d93
Jozef Bíroš <jozef.biros91@gmail.com>
2016-02-25 20:59:17 +0100

    Pridanie do databazy employees column Mobile_Number

--- dbconnect.php
+++ dbconnect.php
@@ -57,6 +57,7 @@
         Password VARCHAR(255) NOT NULL,
         Email VARCHAR(255) NOT NULL,
         Permissions VARCHAR(10) NOT NULL,
+        Mobile_Number INT(255) NOT NULL,
         Reset_Password VARCHAR(50) NOT NULL DEFAULT 0,
         Session_ID VARCHAR(255) NOT NULL,
         UNIQUE (`Email`,`User_ID`) 
