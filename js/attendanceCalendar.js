b4d7d7d19071865be2a894d01aba3117f6590ba8
Bubo <bubomirxxx@gmail.com>
2016-02-26 15:36:21 +0100

    comit

--- js/attendanceCalendar.js
+++ js/attendanceCalendar.js
@@ -52,6 +52,7 @@ $(document).ready(function () {
             }
         });
 
+
         $('#calendar').fullCalendar('addEventSource', JSON.parse(freshevents));
     }
     /*************************************************/
