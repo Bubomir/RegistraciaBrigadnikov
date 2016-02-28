5e8d88ed0b55e0a25e728d43f8d4268a85646c69
Jozef Bíroš <jozef.biros91@gmail.com>
2016-02-28 23:41:56 +0100

    UI improvements

--- js/attendanceCalendar.js
+++ js/attendanceCalendar.js
@@ -211,7 +211,13 @@ $(document).ready(function () {
             refreshEvents();
         }
         else{
-            window.alert("najprv pridaj majstra");
+            refreshEvents();
+            swal({
+                title: "Chyba...",
+                text: "Aby bylo možné přidat brigádníky je zapotřebí vytvořena změna!",
+                type: "error",
+                confirmButtonColor: "#d62633"
+            });
         }
     }
 
@@ -500,7 +506,7 @@ $(document).ready(function () {
                     $('#popup-info').fadeIn('500');
                     $('#popup-info').fadeTo('10', 1.9);
                 }).mousemove(function (e) {
-                    $('#popup-info').css('top', e.pageY + 10);
+                    $('#popup-info').css('top', e.pageY -110);
                     $('#popup-info').css('left', e.pageX + 20);
                 });
             }
@@ -850,7 +856,12 @@ $(document).ready(function () {
                                 }
                             });
                             if (checkingForDelete.responseText === 'supervizor') {
-                                window.alert('nemas prava na vymazamnioe');
+                                swal({
+                                    title: "Chyba...",
+                                    text: "Pouze správce může změnit nebo vymazat změny!",
+                                    type: "error",
+                                    confirmButtonColor: "#d62633"
+                                });
                             } else {
                                 deleteEvent(event, permissions);
 
